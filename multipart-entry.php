0cb821138531d975690bec45ae6a754d5de81a2a
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-26 20:46:43 +0000

    multipart entry
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/lib/multipart-entry/trunk@1568 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ multipart-entry.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Multipart Entry Form
+ * Author: Denis de Bernardy <http://www.mesoconcepts.com>
+ * Version: 1.0
+ */
+
+ob_start('ob_multipart_entry');
+
+add_action('edit_form_advanced', 'add_file_max_size');
+add_action('edit_page_form', 'add_file_max_size');
+
+/**
+ * ob_multipart_entry()
+ *
+ * @param string $buffer Output buffer
+ * @return string $buffer Modified buffer
+ **/
+
+function ob_multipart_entry($buffer) {
+	return str_replace(
+		'<form name="post"',
+		'<form enctype="multipart/form-data" name="post"',
+		$buffer
+		);
+} # ob_multipart_entry()
+
+
+if ( !function_exists('add_max_file_size') ) :
+/**
+ * add_max_file_size()
+ *
+ * @return void
+ **/
+
+function add_max_file_size() {
+	$bytes = apply_filters( 'import_upload_size_limit', wp_max_upload_size() );
+	
+	echo  "\n" . '<input type="hidden" name="MAX_FILE_SIZE" value="' . $bytes .'" />' . "\n";
+} # add_max_file_size()
+endif;
+?>
